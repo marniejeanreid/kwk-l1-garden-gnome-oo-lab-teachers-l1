365e6bf4e7a262b08e2deae52f1ba290d71eaeec
Marnie Reid <marniejeanreid@gmail.com>
2018-07-27 01:17:42 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -50,5 +50,9 @@ class GardenGnome
     return "GNARLY!!!"
   end
   
+  def introduce_self
+    return "Hello humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
+  end
+  
 end
 
